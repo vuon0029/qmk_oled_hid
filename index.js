4ac4a44aff352a6d07117359122d9d9e3504c2e1
Vuong Tuan Kiet <55266028+vuon0029@users.noreply.github.com>
2021-09-05 15:54:55 -0400

    modified for Mercutio

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 #!/usr/bin/env node
 "use strict";
 
+// UPDATE THIS TO FIT YOUR KEYBOARD, INFO CAN BE FOUND IN QMK congif.h FILE
+const KEYBOARD_NAME = "Mercutio"; // #define PRODUCT
+const KEYBOARD_USAGE_ID = 0x61; // default, dont need to change
+const VENDOR_ID = 0x6d77; // #define VENDOR_ID
+const PRODUCT_ID = 0x1703; // #define PRODUCT_ID
+const KEYBOARD_USAGE_PAGE = 0xff60; // default, dont need to change
+const KEYBOARD_UPDATE_TIME = 1000;
+
 const hid = require("node-hid");
+const process = require("process");
 const os = require("os-utils");
 const request = require("request");
 const batteryLevel = require("battery-level");
 const loudness = require("loudness");
+const notifier = require("node-notifier");
+const path = require("path");
+const weatherURL =
+  "https://www.yahoo.com/news/weather/canada/ottawa/ottawa-24017230";
 
 // the node-audio-windows version is much faster on windows, but loudness handles other os's better, so let's get the best of both worlds
 let winAudio;
@@ -15,17 +28,21 @@ try {
   // do nothing
 }
 
-// Keyboard info
-const KEYBOARD_NAME = "Lily58";
-const KEYBOARD_USAGE_ID = 0x61;
-const KEYBOARD_USAGE_PAGE = 0xff60;
-const KEYBOARD_UPDATE_TIME = 1000;
+let kbInterval;
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  `Wednesday`,
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 // Info screen types
-const SCREEN_PERF = 0;
-const SCREEN_STOCK = 1;
-const SCREEN_WEATHER = 2;
-const screens = ["", "", ""];
+const SCREEN_PERF = 2;
+const SCREEN_WEATHER = 3;
+const screens = ["", "", "", ""];
 let currentScreenIndex = 0;
 
 let keyboard = null;
@@ -56,89 +73,24 @@ async function startPerfMonitor() {
     ]);
 
     const screen = [
-      ["CPU:", cpuUsagePercent],
-      ["RAM:", usedMemoryPercent],
-      ["VOL:", volumeLevelPercent],
-      ["BAT:", batteryPercent],
+      ["C:", cpuUsagePercent],
+      ["R:", usedMemoryPercent],
+      ["V:", volumeLevelPercent],
+      ["B:", batteryPercent],
     ];
 
-    const maxTitleSize = Math.max(...screen.map(([header]) => header.length));
-    const barGraphSize = 21 - maxTitleSize - 3;
+    const barGraphSize = 6;
 
     // Set this to be the latest performance info
     screens[SCREEN_PERF] = screen
-      .map(([header, percent], index) => {
+      .map(([header, percent]) => {
         const numBlackTiles = barGraphSize * (percent / 100);
         return `${header} ${"\u0008".repeat(
           Math.ceil(numBlackTiles)
-        )}${" ".repeat(barGraphSize - numBlackTiles)}|${title(index, 0)}`;
+        )}${" ".repeat(barGraphSize - numBlackTiles)}| `;
       })
       .join("");
 
-    await wait(KEYBOARD_UPDATE_TIME);
-  }
-}
-
-async function startStockMonitor() {
-  // Set the stocks that we want to show
-  const stocks = new Map();
-  let counter = 0;
-  stocks.set("MSFT", 0);
-  stocks.set("TSLA", 0);
-  stocks.set("GOOG", 0);
-  stocks.set("FB", 0);
-
-  // The regex used to grab the price from the yahoo stocks page
-  const priceRegex = /"currentPrice":({[^}]+})/;
-
-  function getStocks() {
-    const promises = [];
-    for (const [key, value] of stocks) {
-      promises.push(
-        new Promise((resolve) => {
-          // Get the stock price page for the current stock
-          request(
-            `https://finance.yahoo.com/quote/${key}/`,
-            (err, res, body) => {
-              // Parse out the price and update the map
-              const result = priceRegex.exec(body);
-              if (result && result.length > 1) {
-                let price = JSON.parse(result[1]).raw;
-                price = price.toFixed(2);
-                stocks.set(key, price);
-              }
-              resolve();
-            }
-          );
-        })
-      );
-    }
-
-    // Wait for all the stocks to be updated
-    return Promise.all(promises);
-  }
-
-  // Just keep updating the data forever
-  while (true) {
-    // Get the current stock prices
-    if (counter % 10 === 0) {
-      await getStocks();
-    }
-    counter++;
-
-    // Create a screen using the stock data
-    const lines = [];
-    for (const [key, value] of stocks) {
-      const line = `${key.padEnd(5)}: $${value}`;
-      lines.push(
-        `${line}${" ".repeat(16 - line.length)}|  ${title(lines.length, 1)} `
-      );
-    }
-
-    // Set this to be the latest stock info
-    screens[SCREEN_STOCK] = lines.join("");
-
-    // Pause a bit before requesting more info
     await wait(KEYBOARD_UPDATE_TIME);
   }
 }
@@ -151,27 +103,24 @@ async function startWeatherMonitor() {
 
   function getWeather() {
     return new Promise((resolve) => {
-      request(
-        `https://www.yahoo.com/news/weather/united-states/st-augustine/st-augustine-12771497`,
-        (err, res, body) => {
-          const weather = {};
-          const temp = tempRegex.exec(body);
-          if (temp && temp.length > 1) {
-            weather.temp = JSON.parse(temp[1]);
-          }
-
-          const cond = condRegex.exec(body);
-          if (cond && cond.length > 1) {
-            weather.desc = cond[1];
-          }
-
-          const rain = rainRegex.exec(body);
-          if (rain && rain.length > 1) {
-            weather.rain = rain[1];
-          }
-          resolve(weather);
+      request(weatherURL, (err, res, body) => {
+        const weather = {};
+        const temp = tempRegex.exec(body);
+        if (temp && temp.length > 1) {
+          weather.temp = JSON.parse(temp[1]);
         }
-      );
+
+        const cond = condRegex.exec(body);
+        if (cond && cond.length > 1) {
+          weather.desc = cond[1];
+        }
+
+        const rain = rainRegex.exec(body);
+        if (rain && rain.length > 1) {
+          weather.rain = rain[1];
+        }
+        resolve(weather);
+      });
     });
   }
 
@@ -208,22 +157,30 @@ async function startWeatherMonitor() {
       }
       lastWeather = weather;
 
-      // Create the new screen
-      const screen =
-        `desc: ${description}${" ".repeat(
-          Math.max(0, 9 - ("" + description).length)
-        )} |  ${title(0, 2)} ` +
-        `temp: ${weather.temp.now}${" ".repeat(
-          Math.max(0, 9 - ("" + weather.temp.now).length)
-        )} |  ${title(1, 2)} ` +
-        `high: ${weather.temp.high}${" ".repeat(
-          Math.max(0, 9 - ("" + weather.temp.high).length)
-        )} |  ${title(2, 2)} ` +
-        `rain: ${weather.rain}%${" ".repeat(
-          Math.max(0, 8 - ("" + weather.rain).length)
-        )} |  ${title(3, 2)} `;
+      let date_ob = new Date();
 
-      // Set this to be the latest weather info
+      let date = ("0" + date_ob.getDate()).slice(-2);
+      let day = date_ob.getDay();
+      let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+      let year = date_ob.getFullYear();
+      let hours = date_ob.getHours();
+      let minutes = date_ob.getMinutes();
+      const ampm = hours >= 12 ? "pm" : "am";
+      hours = hours % 12;
+      hours = hours ? hours : 12; // the hour '0' should be '12'
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      const timeString = hours + ":" + minutes + ampm;
+
+      const dateString = `${date}/${month}/${year}`;
+
+      const screen =
+        `${dateString}${" ".repeat(
+          Math.max(0, 9 - ("" + dateString).length)
+        )}    ` +
+        `${timeString}${" ".repeat(
+          Math.max(0, 4 - ("" + timeString).length)
+        )}  ${days[day]}       `;
+
       screens[SCREEN_WEATHER] = screen;
     }
 
@@ -232,20 +189,13 @@ async function startWeatherMonitor() {
   }
 }
 
-function title(i, titleIndex) {
-  // Return the character that indicates the title part from the font data
-  if (i === 3) {
-    return "\u00DE";
-  }
-  return String.fromCharCode(0x9a - titleIndex + i * 32);
-}
-
 async function sendToKeyboard(screen) {
   // If we are already buffering a screen to the keyboard just quit early.
   // Or if there is no update from what we sent last time.
   if (screenBuffer || screenLastUpdate === screen) {
     return;
   }
+  console.log("Sending updates... ");
 
   screenLastUpdate = screen;
 
@@ -258,47 +208,82 @@ async function sendToKeyboard(screen) {
   // Split the bytes into 4 lines that we will send one at a time
   // This is to prevent hitting the 32 length limit on the connection
   const lines = [];
-  lines.push([0].concat(screenBuffer.slice(0, 21)));
-  lines.push([0].concat(screenBuffer.slice(21, 42)));
-  lines.push([0].concat(screenBuffer.slice(42, 63)));
-  lines.push([0].concat(screenBuffer.slice(63, 84)));
+
+  lines.push([0].concat(screenBuffer.slice(0, 32)));
 
   // Loop through and send each line after a small delay to allow the
   // keyboard to store it ready to send to the slave side once full.
-  let index = 0;
-  for (const line of lines) {
-    if (os.platform() === "darwin") {
-      await wait(100);
+  try {
+    let index = 0;
+    for (const line of lines) {
+      if (os.platform() === "darwin") {
+        await wait(200);
+      }
+      const data = await keyboard.write(line);
+      console.log("written: ", data);
+      if (os.platform() === "darwin") {
+        await wait(200);
+      } else {
+        await wait(100);
+      }
     }
-    keyboard.write(line);
-    if (os.platform() === "darwin") {
-      await wait(100);
-    } else {
-      await wait(20);
-    }
-  }
 
-  // We have sent the screen data, so clear it ready for the next one
-  screenBuffer = null;
+    // We have sent the screen data, so clear it ready for the next one
+    screenBuffer = null;
+  } catch (err) {
+    console.log("ERR: ", err);
+    notifier.notify(
+      {
+        title: "Keyboard: Failed to write HID",
+        message: "Click to close server!",
+        icon: path.join(__dirname, "coulson.jpg"), // Absolute path (doesn't work on balloons)
+        sound: true, // Only Notification Center or Windows Toasters
+        wait: true, // Wait with callback, until user action is taken against notification, does not apply to Windows Toasters as they always wait or notify-send as it does not support the wait option
+      },
+      function (err, response, metadata) {
+        // Response is response from notification
+        // Metadata contains activationType, activationAt, deliveredAt
+      }
+    );
+
+    notifier.on("click", function (notifierObject, options, event) {
+      // Triggers if `wait: true` and user clicks notification
+
+      console.log("close");
+      keyboard.close();
+      clearInterval(kbInterval);
+      kbInterval = 0;
+      process.exit(1);
+    });
+
+    notifier.on("timeout", function (notifierObject, options) {
+      // Triggers if `wait: true` and notification closes
+      console.log("close");
+      keyboard.close();
+      clearInterval(kbInterval);
+      kbInterval = 0;
+      process.exit(1);
+    });
+  }
 }
 
-function updateKeyboardScreen() {
+async function updateKeyboardScreen() {
   // If we don't have a connection to a keyboard yet, look now
   if (!keyboard) {
-    // Search all devices for a matching keyboard
     const devices = hid.devices();
     for (const d of devices) {
       if (
         d.product === KEYBOARD_NAME &&
         d.usage === KEYBOARD_USAGE_ID &&
-        d.usagePage === KEYBOARD_USAGE_PAGE
+        d.usagePage === KEYBOARD_USAGE_PAGE &&
+        d.vendorId === VENDOR_ID &&
+        d.productId === PRODUCT_ID
       ) {
-        // Create a new connection and store it as the keyboard
         keyboard = new hid.HID(d.path);
         console.log(`Keyboard connection established.`);
 
-        // Listen for data from the keyboard which indicates the screen to show
         keyboard.on("data", (e) => {
+          // console.log(e);
           // Check that the data is a valid screen index and update the current one
           if (e[0] >= 1 && e[0] <= screens.length) {
             currentScreenIndex = e[0] - 1;
@@ -312,23 +297,64 @@ function updateKeyboardScreen() {
         // 1st byte - unused and thrown away on windows see bug in node-hid
         // 2nd byte - 1 to indicate a new connection
         // 3rd byte - number of screens the keyboard can scroll through
-        keyboard.write([0, 1, screens.length]);
+        // let initialBuffer = [];
+        // for (let i = 0; i < initialByte.length; i++) {
+        //   initialBuffer.push(initialByte.charAt(i));
+        // }
+
+        const initialBytes = [
+          0x00,
+          1,
+          screens.length,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+          0,
+        ];
+
+        keyboard.write(initialBytes);
+
         break;
       }
     }
   }
 
   // If we have a connection to a keyboard and a valid screen
-  if (keyboard && screens[currentScreenIndex].length === 84) {
+  if (keyboard && screens[currentScreenIndex].length > 0) {
     // Send that data to the keyboard
     sendToKeyboard(screens[currentScreenIndex]);
   }
 }
 
-// Start the monitors that collect the info to display
+// Start the monitors that collect the info to display in the background
 startPerfMonitor();
-startStockMonitor();
 startWeatherMonitor();
 
 // Update the data on the keyboard with the current info screen every second
-setInterval(updateKeyboardScreen, KEYBOARD_UPDATE_TIME);
+kbInterval = setInterval(updateKeyboardScreen, KEYBOARD_UPDATE_TIME);
